c163b0b29bc284c7a80262d9dc16e850bd28a19d
Anton Mironov <ant.mironov@gmail.com>
2009-10-10 18:34:42 +0600

    Beautified installation process

--- install.rb
+++ install.rb
@@ -4,12 +4,16 @@ require 'ftools'
 destination_file = File.expand_path("#{File.dirname(__FILE__)}/../../../public/javascripts/in_place_editor_with_empty_text.js")
 file = "#{File.dirname(__FILE__)}/in_place_editor_with_empty_text.js"
 
+puts
+puts "Because of buggy Ajax.InPlaceEditor class in script.aculo.us,"
+puts "we're going to define our own Ajax.InPlaceEditorWithEmptyText."
+puts "Copying #{file} to #{destination_file}..."
+puts
 if File::exists? destination_file
-  puts "#{destination_file} already exists. Please ensure it is up-to-date."
-  puts "See #{file}"
+  puts "in_place_editor_with_empty_text.js is already exists in javascripts dir."
+  puts "Please ensure it is up-to-date."
+  puts "See vendor/plugins/in_place_editing/in_place_editor_with_empty_text.js"
 else  
-  puts "Beacause of buggy Ajax.InPlaceEditor class in script.aculo.us,"
-  puts "we're going to define our own Ajax.InPlaceEditorWithEmptyText."
-  puts "Copying #{file} to #{destination_file}..."
   File.copy file, destination_file
+  puts "Completed."
 end
