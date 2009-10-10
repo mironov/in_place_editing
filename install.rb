99499d9fc64866044a86d1048a83b34cbbdf0cb9
Anton Mironov <ant.mironov@gmail.com>
2009-10-10 18:36:28 +0600

    Once more

--- install.rb
+++ install.rb
@@ -7,7 +7,7 @@ file = "#{File.dirname(__FILE__)}/in_place_editor_with_empty_text.js"
 puts
 puts "Because of buggy Ajax.InPlaceEditor class in script.aculo.us,"
 puts "we're going to define our own Ajax.InPlaceEditorWithEmptyText."
-puts "Copying #{file} to #{destination_file}..."
+puts "Copying in_place_editor_with_empty_text.js to javascripts dir..."
 puts
 if File::exists? destination_file
   puts "in_place_editor_with_empty_text.js is already exists in javascripts dir."
