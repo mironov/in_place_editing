e1b7ca744af36f7e614e6428701b7b75710a644f
Anton Mironov <ant.mironov@gmail.com>
2010-07-25 22:45:57 +0600

    Fixed ability to change "click to edit" text

--- lib/in_place_macros_helper.rb
+++ lib/in_place_macros_helper.rb
@@ -58,7 +58,7 @@ module InPlaceMacrosHelper
     js_options['ajaxOptions'] = options[:options] if options[:options]
     js_options['htmlResponse'] = !options[:script] if options[:script]
     js_options['callback']   = "function(form) { return #{options[:with]} }" if options[:with]
-    js_options['clickToEditText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
+    js_options['emptyText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
     js_options['textBetweenControls'] = %('#{options[:text_between_controls]}') if options[:text_between_controls]
     function << (', ' + options_for_javascript(js_options)) unless js_options.empty?
     
